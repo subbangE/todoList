34251b2103c11472ab2b13865d6885727980a305
SUBIN <ksb010105@naver.com>
2024-07-15 13:24:04 +0900

    최초 커밋

---
+++ src/main/java/com/mysite/todolist/TodoListApplication.java
@@ -0,0 +1,13 @@
+package com.mysite.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoListApplication.class, args);
+    }
+
+}

7e1410b2599be0afb0defc1de0136c456edad4b5
SUBIN <ksb010105@naver.com>
2024-07-17 11:58:08 +0900

    todoList 체크박스 수정 구현

--- src/main/java/com/mysite/todolist/controller/TodoController.java
+++ src/main/java/com/mysite/todolist/controller/TodoController.java
@@ -1,7 +1,6 @@
 package com.mysite.todolist.controller;
 
 import com.mysite.todolist.model.Todo;
-import com.mysite.todolist.repository.TodoRepository;
 import com.mysite.todolist.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,32 +10,31 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping("/todo")
 public class TodoController {
 
     @Autowired
     private TodoService todoService;
 
-    @GetMapping({"","/"})
+    @GetMapping({"","/", "/todo"})
     public String showTodoList(Model model) {
         List<Todo> todos = todoService.getTodoList();
         model.addAttribute("todos", todos);
         return "index";
     }
 
-    @PostMapping("/add")
+    @PostMapping("/todo/add")
     public String add(@RequestParam String content) {
         todoService.todoCreate(content);
         return "redirect:/todo";
     }
 
-    @PostMapping("/delete/{id}")
+    @PostMapping("/todo/delete/{id}")
     public String delete(@PathVariable Integer id) {
         todoService.todoDelete(id);
         return "redirect:/todo";
     }
 
-    @PostMapping("/update/{id}")
+    @PostMapping("/todo/update/{id}")
     public String update(@PathVariable Integer id, @RequestParam boolean completed) {
         todoService.todoUpdate(id, completed);
         return "redirect:/todo";
